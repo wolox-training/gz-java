73fe110123442cfb4a458a9a4da6a8844171d407
Gonzalo Zamudio <gonzalo.zamudio@wolox.com.ar>
2019-07-01 14:53:41 -0300

    Added book model

---
+++ src/main/java/wolox/training/models/Book.java
@@ -0,0 +1,118 @@
+package wolox.training.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Book {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private long id;
+
+  @Column
+  private String genre;
+
+  @Column(nullable = false)
+  private String author;
+
+  @Column(nullable = false)
+  private String image;
+
+  @Column(nullable = false)
+  private String title;
+
+  @Column(nullable = false)
+  private String subtitle;
+
+  @Column(nullable = false)
+  private String publisher;
+
+  @Column(nullable = false)
+  private String year;
+
+  @Column(nullable = false)
+  private int pages;
+
+  @Column(nullable = false)
+  private String isbn;
+
+  public Book() {
+
+  }
+
+  public String getGenre() {
+    return this.genre;
+  }
+
+  public void setGenre(String genre) {
+    this.genre = genre;
+  }
+
+  public String getImage() {
+    return this.image;
+  }
+
+  public void setImage(String image) {
+    this.image = image;
+  }
+
+  public String getTitle() {
+    return this.title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public String getSubtitle() {
+    return this.subtitle;
+  }
+
+  public void setSubtitle(String subtitle) {
+    this.subtitle = subtitle;
+  }
+
+  public String getPublisher() {
+    return this.publisher;
+  }
+
+  public void setPublisher(String publisher) {
+    this.publisher = publisher;
+  }
+
+  public String getYear() {
+    return this.year;
+  }
+
+  public void setYear(String year) {
+    this.year = year;
+  }
+
+  public int getPages() {
+    return this.pages;
+  }
+
+  public void setPages(int pages) {
+    this.pages = pages;
+  }
+
+  public String getIsbn() {
+    return this.isbn;
+  }
+
+  public void setIsbn(String isbn) {
+    this.isbn = isbn;
+  }
+
+  public String getAuthor() {
+    return this.author;
+  }
+
+  public void setAuthor(String author) {
+    this.author = author;
+  }
+}
