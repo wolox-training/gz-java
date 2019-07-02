8dac741bdc1cf2caf213092fde1a25bc06a5b2ff
Gonzalo Zamudio <gonzalo.zamudio@wolox.com.ar>
2019-07-02 15:13:24 -0300

    Fixes of rebase

--- src/main/java/wolox/training/repositories/UsersRepository.java
+++ src/main/java/wolox/training/repositories/UsersRepository.java
@@ -3,6 +3,6 @@ package wolox.training.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import wolox.training.models.Users;
 
-public interface UsersRepository extends JpaRepository<Users, Integer> {
+public interface UsersRepository extends JpaRepository<Users, Long> {
   Users findByUsername(String username);
 }
