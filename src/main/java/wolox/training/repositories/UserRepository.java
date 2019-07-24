2fc36e6a14361da2709702885901af992f999930
Gonzalo Zamudio <gonzalo.zamudio@wolox.com.ar>
2019-07-24 14:53:15 -0300

    Added find methods to repositories

--- src/main/java/wolox/training/repositories/UserRepository.java
+++ src/main/java/wolox/training/repositories/UserRepository.java
@@ -1,8 +1,11 @@
 package wolox.training.repositories;
 
+import java.time.LocalDate;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import wolox.training.models.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
   User findByUsername(String username);
+  List<User> findByNameContainingIgnoreCaseAndBirthdayBetween(String name, LocalDate startDate, LocalDate endDate);
 }
