3b2ee5d3fe01e8d573c6bad6f706e5448556eba2
Gonzalo Zamudio <gonzalo.zamudio@wolox.com.ar>
2019-07-25 15:41:34 -0300

    request param

--- src/main/java/wolox/training/repositories/BookRepository.java
+++ src/main/java/wolox/training/repositories/BookRepository.java
@@ -1,9 +1,11 @@
 package wolox.training.repositories;
 
 import java.util.List;
+import java.util.Map;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.web.bind.annotation.RequestParam;
 import wolox.training.models.Book;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
@@ -15,4 +17,6 @@ public interface BookRepository extends JpaRepository<Book, Long> {
       + "(b.year = :year or b.year is null)")
   List<Book> findByGenreAndPublisherAndYear(@Param("genre") String genre,
       @Param("publisher") String publisher, @Param("year") String year);
+
+  List<Book> getAll(@RequestParam Map<String,String> allParams);
 }
